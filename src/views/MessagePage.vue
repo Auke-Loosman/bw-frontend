e3f17f84638694c210e926dd4838ebf1029b4113
Auke <auke@skrepr.com>
2026-02-19 12:20:08 +0100

    Add filtering with computed property and watcher

--- src/views/MessagePage.vue
+++ src/views/MessagePage.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import type { Message } from '@/types/Message'
+import { ref, onMounted, computed, watch } from 'vue'
+import type { Message, MessageType } from '@/types/Message'
 import { messageService } from '@/services/messageService'
 
 const messages = ref<Message[]>([])
 const loading = ref<boolean>(false)
+
+const selectedType = ref<MessageType | 'all'>('all')
 
 const fetchMessages = async () => {
   loading.value = true
@@ -15,6 +17,18 @@ const fetchMessages = async () => {
   }
 }
 
+const filteredMessages = computed(() => {
+  if (selectedType.value === 'all') {
+    return messages.value
+  }
+
+  return messages.value.filter((message) => message.type === selectedType.value)
+})
+
+watch(selectedType, (newValue) => {
+  console.log('Filter changed to:', newValue)
+})
+
 onMounted(() => {
   fetchMessages()
 })
@@ -24,10 +38,20 @@ onMounted(() => {
   <div>
     <h1>Messages</h1>
 
+    <label>
+      Filter by type:
+      <select v-model="selectedType">
+        <option value="all">All</option>
+        <option value="incoming">Incoming</option>
+        <option value="outgoing">Outgoing</option>
+        <option value="task">Task</option>
+      </select>
+    </label>
+
     <div v-if="loading">Loading...</div>
 
     <ul v-else>
-      <li v-for="message in messages" :key="message.id">
+      <li v-for="message in filteredMessages" :key="message.id">
         {{ message.subject }} - {{ message.type }}
       </li>
     </ul>
